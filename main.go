4d33a9f68c42c3f3df341154c04802ff89ef7e73
meenachinmay <Chinmayanand896@gmail.com>
2024-01-16 10:02:29 +0900

    get mouse state, update the box state with mouse state

--- main.go
+++ main.go
@@ -1,48 +1,8 @@
-// package main
-
-// import "github.com/veandco/go-sdl2/sdl"
-
-// func main() {
-// 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
-// 		panic(err)
-// 	}
-// 	defer sdl.Quit()
-
-// 	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-// 		800, 600, sdl.WINDOW_SHOWN)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer window.Destroy()
-
-// 	surface, err := window.GetSurface()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	surface.FillRect(nil, 0)
-
-// 	rect := sdl.Rect{X: 0, Y: 0, W: 200, H: 200}
-// 	colour := sdl.Color{R: 255, G: 0, B: 255, A: 255} // purple
-// 	pixel := sdl.MapRGBA(surface.Format, colour.R, colour.G, colour.B, colour.A)
-// 	surface.FillRect(&rect, pixel)
-// 	window.UpdateSurface()
-
-// 	running := true
-// 	for running {
-// 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-// 			switch event.(type) {
-// 			case *sdl.QuitEvent:
-// 				println("Quit")
-// 				running = false
-// 				break
-// 			}
-// 		}
-// 	}
-// }
-
 package main
 
 import (
+	"fmt"
+
 	"github.com/veandco/go-sdl2/sdl"
 )
 
@@ -123,6 +83,12 @@ func (g *game) handleEvents() {
 
 func (g *game) update() {
 	// Any game logic updates would go here
+	x, y, null := sdl.GetMouseState()
+	fmt.Println(x, y, null)
+
+	// updatet the box state
+	g.boxX = x
+	g.boxY = y
 }
 
 func (g *game) render() {
