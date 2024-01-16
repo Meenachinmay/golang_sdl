e3693a003f8d20a7626463947a3e7f78102b5322
meenachinmay <Chinmayanand896@gmail.com>
2024-01-17 08:37:47 +0900

    added folders

--- pkg/game/update.go
+++ pkg/game/update.go
@@ -1,6 +1,10 @@
 // update.go
 package main
 
+import (
+    "github.com/meenachinmay/golang_sdl/pkg/utils/constants"
+)
+
 func (g *Game) update(deltaTime float32) {
     g.boxX += g.velX * boxSpeed * deltaTime
     g.boxY += g.velY * boxSpeed * deltaTime
